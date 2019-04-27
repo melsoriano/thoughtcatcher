643d435bd389132944e8b6a4da921a8abba33571
mellaniesoriano <23709723+mellaniesoriano@users.noreply.github.com>
2019-04-26 20:04:35 -1000

    Notes can now be edited and saved

--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -6,6 +6,7 @@ import NotFound from './containers/NotFound';
 import Login from './containers/Login';
 import Signup from './containers/Signup';
 import NewNote from './containers/NewNote';
+import Notes from './containers/Notes';
 
 export default ({ childProps }) => (
   <Switch>
@@ -16,6 +17,12 @@ export default ({ childProps }) => (
       path="/notes/new"
       exact
       component={NewNote}
+      props={childProps}
+    />
+    <AppliedRoute
+      path="/notes/:id"
+      exact
+      component={Notes}
       props={childProps}
     />
     {/* Finally, catch all unmatched routes */}
