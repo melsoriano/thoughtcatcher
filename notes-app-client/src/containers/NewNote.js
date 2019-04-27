9865660e80626e3cf7b0ac7b8d8cbea21dbe2fe1
mellaniesoriano <23709723+mellaniesoriano@users.noreply.github.com>
2019-04-26 19:37:11 -1000

    Created new note component

--- notes-app-client/src/containers/NewNote.js
+++ notes-app-client/src/containers/NewNote.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+import { API } from 'aws-amplify';
 import LoaderButton from '../components/LoaderButton';
 import config from '../config';
 import './NewNote.css';
+import { s3Upload } from '../libs/awsLib';
 
 export default class NewNote extends Component {
   constructor(props) {
@@ -39,7 +41,21 @@ export default class NewNote extends Component {
       return;
     }
     this.setState({ isLoading: true });
+
+    try {
+      const attachment = this.file ? await s3Upload(this.file) : null;
+
+      await this.createNote({ attachment, content: this.state.content });
+      this.props.history.push('/');
+    } catch (e) {
+      alert(e);
+      this.setState({ isLoading: false });
+    }
   };
+
+  createNote(note) {
+    return API.post('notes', '/notes', { body: note });
+  }
 
   render() {
     return (
