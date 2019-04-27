bc4a1be3d33199113d42c83eb25b13bf1472c63e
mellaniesoriano <23709723+mellaniesoriano@users.noreply.github.com>
2019-04-26 18:06:32 -1000

    Connected aws auth and created logout function

--- notes-app-client/src/App.js
+++ notes-app-client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import { Nav, Navbar, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
@@ -6,7 +6,26 @@ import './App.css';
 import Routes from './Routes';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAuthenticated: false,
+    };
+  }
+
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  };
+
+  handleLogout = event => {
+    this.userHasAuthenticated(false);
+  };
+
   render() {
+    const authProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated,
+    };
     return (
       <div className="App container">
         <Navbar fluid collapseOnSelect>
@@ -18,16 +37,22 @@ class App extends Component {
           </Navbar.Header>
           <Navbar.Collapse>
             <Nav pullRight>
-              <LinkContainer to="/signup">
-                <NavItem>Signup</NavItem>
-              </LinkContainer>
-              <LinkContainer to="/login">
-                <NavItem>Login</NavItem>
-              </LinkContainer>
+              {this.state.isAuthenticated ? (
+                <NavItem onClick={this.handleLogout}>Logout</NavItem>
+              ) : (
+                <Fragment>
+                  <LinkContainer to="/signup">
+                    <NavItem>Signup</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <NavItem>Login</NavItem>
+                  </LinkContainer>
+                </Fragment>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-        <Routes />
+        <Routes childProps={authProps} />
       </div>
     );
   }
